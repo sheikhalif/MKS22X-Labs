f4080cc13620c27ef33fb65db4a9c2221a03e676
Sheikh Alif <salif30@stuy.edu>
2022-02-18 10:10:53 -0500

    debugging the size parameters

--- Classwork/ReadFile.java
+++ Classwork/ReadFile.java
@@ -3,47 +3,60 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class ReadFile{
   //method to read file
-  public static int[] getValuesFromFile{
-    File text = new File (filename);
-    int rows = 0;
-    int col = 0;
-    //gets the number of rows
-    while (text.hasNextLine()){
-      rows++;
-    }
-
-    //gets number of cols
-    String nextLine = text.nextLine();
-    col = nextLine.string();
-
-    //sets size of int array
-    int[][] answer = new int[rows][col];
-
-    //uses current line before looping through the rest of the lines
-    for (int a = 0; a<cols; a++){
-      answer[0][a] = nextLine.charAt(a);
-    }
-
-    //put in all the values into 2d character array
-    for (int i = 1; i < rows; i++){
-      nextLine = text.nextLine();
-      for (int x = 0; x < col; x++){
-        answer[i][x] = nextLine.charAt(x);
+  public static int[][] getValuesFromFile(String filename){
+    try{
+      System.out.println("Method triggered");
+      File textOld = new File (filename);
+      Scanner text = new Scanner(textOld);
+      int rows = 0;
+      int col = 0;
+      //gets the number of rows
+      while (text.hasNextLine()){
+        rows++;
+        System.out.println("Rows: " + rows);
       }
-    }
+      System.out.println("Rows: " + rows);
 
-    return answer;
+      //gets number of cols
+      String nextLine = text.nextLine();
+      col = nextLine.length();
+      System.out.println("Cols: " + col);
+
+      //sets size of int array
+      int[][] answer = new int[rows][col];
+
+      //uses current line before looping through the rest of the lines
+      for (int a = 0; a<col; a++){
+        answer[0][a] = nextLine.charAt(a);
+      }
+
+      //put in all the values into 2d character array
+      for (int i = 1; i < rows; i++){
+        nextLine = text.nextLine();
+        for (int x = 0; x < col; x++){
+          answer[i][x] = nextLine.charAt(x);
+        }
+      }
+
+      return answer;
+    }
+    catch(FileNotFoundException e){
+      int[][] answer = new int[0][0];
+      System.out.println("File name is not valid");
+      return answer;
+    }
 
   }
 
   //main - check if file is valid
   public static void main(String args[]){
-    int[] nums = new int[0];
+    int[][] nums = new int[0][0];
     try{
-      nums = getValuesFromFile("Numbers.csv");
+      nums = getValuesFromFile("Maze1.txt");
+      System.out.println(nums.toString());
     }
-    catch(FileNotFoundException e){
-      System.out.println("File is not valid")
+    catch(Exception e){
+      System.out.println("File is not valid");
     }
   }
 }
