2b5d14b4a4b6880bda6fd8a0c3b24886e600498e
Sheikh Alif <salif30@stuy.edu>
2022-03-06 11:17:33 -0500

    still debugging maze generator, had to add a helper function to create the 2d arrays and a helper function to turn the 2d array in a string

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,10 +1,11 @@
 import java.util.*;
 import java.io.*;
 
-public MazeGenerator{
+public class MazeGenerator{
   public static void generate(char[][]maze, int startrow, int startcol){
     maze[startrow][startcol] = 'S';
     if (startrow+1 < maze.length && startcol+1 < maze[0].length){
+      System.out.println(arrayToString(maze));
       if (maze[startrow][startcol] == '#'){
         maze[startrow][startcol] = ' ';
         Random rand = new Random();
@@ -23,5 +24,35 @@ public MazeGenerator{
         }
       }
     }
+  }
+
+  public static void main(String args[]){
+    char[][] maze1 = mazeArrayMaker(7, 7);
+    System.out.println(arrayToString(maze1));
+    generate(maze1, 2, 3);
+    System.out.println(arrayToString(maze1));
+  }
+
+  public static char[][] mazeArrayMaker(int rows, int cols){
+    char[][] answer = new char[rows][cols];
+    for (int i = 0; i < rows; i++){
+      for (int x = 0; x < cols; x++){
+        answer[i][x] = '#';
+      }
+    }
+    return answer;
+  }
+
+  public static String arrayToString(char[][] maze){
+    String answer = "";
+    int rows = maze.length;
+    int cols = maze[0].length;
+    for (int i = 0; i < rows; i++){
+      for (int x = 0; x < cols; x++){
+        answer+= maze[i][x];
+      }
+      answer+="\n";
+    }
+    return answer;
   }
 }
