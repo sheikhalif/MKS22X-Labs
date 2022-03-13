7e2c56107cd9c03ae74354a52e02207c10b3d1bd
Sheikh Alif <salif30@stuy.edu>
2022-03-13 12:47:05 -0400

    fixed depth bugs, fixed how the function handles exceptions. done with bronze

--- 04-usaco/Bronze.java
+++ 04-usaco/Bronze.java
@@ -2,73 +2,66 @@ import java.io.*;
 import java.util.*;
 
 public class Bronze{
-  public static long solve(String filename) throws FileNotFoundException{
-    File obj = new File(filename);
-    Scanner scanner1 = new Scanner(obj);
-    int rows = scanner1.nextInt();
-    int cols = scanner1.nextInt();
-    int waterLevel = scanner1.nextInt();
-    int numCommands = scanner1.nextInt();
-    int[][] field = new int[rows][cols];
-    for (int i = 0; i < rows; i++){
-      for (int x = 0; x < cols; x++){
-        field[i][x] = scanner1.nextInt();
+  public static long solve(String filename){
+    try{
+      File obj = new File(filename);
+      Scanner scanner1 = new Scanner(obj);
+      int rows = scanner1.nextInt();
+      int cols = scanner1.nextInt();
+      int waterLevel = scanner1.nextInt();
+      int numCommands = scanner1.nextInt();
+      int[][] field = new int[rows][cols];
+      for (int i = 0; i < rows; i++){
+        for (int x = 0; x < cols; x++){
+          field[i][x] = scanner1.nextInt();
+        }
       }
-    }
-    System.out.println(numCommands);
-    for (int i = 0; i < numCommands; i++){
-      int commandRow = scanner1.nextInt()-1;
-      int commandCol = scanner1.nextInt()-1;
-      int difference = scanner1.nextInt();
-      System.out.println(commandRow);
-      System.out.println(commandCol);
-      System.out.println(difference);
-      int largestNum = -10000000;
-      for (int a = 0; a < 3; a++){
-        for (int b = 0; b < 3; b++){
-          if (field[commandRow+a][commandCol+b] > largestNum){
-            largestNum = field[commandRow+a][commandCol+b];
+      for (int i = 0; i < numCommands; i++){
+        int commandRow = scanner1.nextInt()-1;
+        int commandCol = scanner1.nextInt()-1;
+        int difference = scanner1.nextInt();
+        int largestNum = -10000000;
+        for (int a = 0; a < 3; a++){
+          for (int b = 0; b < 3; b++){
+            if (field[commandRow+a][commandCol+b] > largestNum){
+              largestNum = field[commandRow+a][commandCol+b];
+            }
+          }
+        }
+        for (int a = 0; a < 3; a++){
+          for (int b = 0; b < 3; b++){
+            if (field[commandRow+a][commandCol+b] >= largestNum - difference){
+              field[commandRow+a][commandCol+b] = largestNum - difference;
+            }
           }
         }
       }
-      for (int a = 0; a < 3; a++){
-        for (int b = 0; b < 3; b++){
-          if (field[commandRow+a][commandCol+b] >= largestNum - difference){
-            field[commandRow+a][commandCol+b] = largestNum - difference;
-          }
-        }
-      }
-      System.out.println(matrixPrint(field));
-    }
 
-    for (int i = 0; i < field.length; i++){
-      for (int x = 0; x < field[0].length; x++){
-        if (field[i][x] - waterLevel > 0){
-          field[i][x] = field[i][x] - waterLevel;
-        }
-        else{
-          field[i][x] = 0;
+      for (int i = 0; i < field.length; i++){
+        for (int x = 0; x < field[0].length; x++){
+          if (waterLevel - field[i][x] > 0){
+            field[i][x] = waterLevel - field[i][x];
+          }
+          else{
+            field[i][x] = 0;
+          }
         }
       }
-    }
-    System.out.println(matrixPrint(field));
-    int answer = 0;
-    for (int i = 0; i < field.length; i++){
-      for (int x = 0; x < field[0].length; x++){
-        answer+= field[i][x];
+      int answer = 0;
+      for (int i = 0; i < field.length; i++){
+        for (int x = 0; x < field[0].length; x++){
+          answer+= field[i][x];
+        }
       }
+      return (answer*72*72);
     }
-    return (answer*72*72);
+    catch (FileNotFoundException e){
+      return -1;
+    }
   }
 
   public static void main(String args[]){
-    try{
-      System.out.println(solve("Example1.txt"));
-    }
-    catch(FileNotFoundException e){
-      System.out.println("File not found");
-    }
-
+    System.out.println(solve("Example1.txt"));
   }
 
   public static String matrixPrint(int[][] x){
