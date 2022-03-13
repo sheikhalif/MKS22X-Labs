8f013647c09882fb0dc4011a6bc1ec69d5057265
Sheikh Alif <salif30@stuy.edu>
2022-03-13 13:44:03 -0400

    added algorithm that was discussed in class. now need to return the correct output

--- 04-usaco/Silver.java
+++ 04-usaco/Silver.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Silver{
-  public static void solve(String filename){
+  public static long solve(String filename){
     try{
       File obj = new File(filename);
       Scanner scanner1 = new Scanner(obj);
@@ -23,10 +23,40 @@ public class Silver{
           }
         }
       }
+      int firstRow = scanner1.nextInt()-1;
+      int firstCol = scanner1.nextInt()-1;
+      int lastRow = scanner1.nextInt()-1;
+      int lastCol = scanner1.nextInt()-1;
+      pasture[firstRow][firstCol] = 1;
+
+      for (int r = 0; r < seconds; r++){
+        int[][] newPasture = copyMatrix(pasture);
+        System.out.println(matrixPrint(pasture));
+        for (int a = 0; a < pasture.length; a++){
+          for (int b = 0; b < pasture[0].length; b++){
+            if (pasture[a][b] >= 1){
+              if (valid(a+1, b, pasture)){
+                newPasture[a+1][b]++;
+              }
+              if (valid(a-1, b, pasture)){
+                newPasture[a-1][b]++;
+              }
+              if (valid(a, b+1, pasture)){
+                newPasture[a][b+1]++;
+              }
+              if (valid(a, b-1, pasture)){
+                newPasture[a][b-1]++;
+              }
+            }
+          }
+        }
+        pasture = newPasture;
+      }
       System.out.println(matrixPrint(pasture));
+      return 1;
     }
     catch(FileNotFoundException e){
-      System.out.println("File not found");
+      return -1;
     }
   }
 
@@ -39,6 +69,26 @@ public class Silver{
       answer+= "\n";
     }
     return answer;
+  }
+
+  public static int[][] copyMatrix(int[][] pasture){
+    int[][] answer = new int[pasture.length][pasture[0].length];
+    for (int i = 0; i < pasture.length; i++){
+      for (int x = 0; x < pasture[0].length; x++){
+        answer[i][x] = pasture[i][x];
+      }
+    }
+    return answer;
+  }
+
+  public static boolean valid(int rowInput, int colInput, int[][] pasture){
+    if (colInput >= pasture[0].length || colInput < 0 || rowInput >= pasture.length || rowInput < 0){
+      return false;
+    }
+    if (pasture[rowInput][colInput] == -1){
+      return false;
+    }
+    return true;
   }
 
   public static void main(String args[]){
