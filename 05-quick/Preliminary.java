8a4df929937a79dd3494bf51feaf8badc738c4f0
Sheikh Alif <salif30@stuy.edu>
2022-03-14 10:13:06 -0400

    end of class period, set up function header

---
+++ 05-quick/Preliminary.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Preliminary{
+  public static int partition(int[] data, int start, int end){
+    Random rand1 = new Random();
+    int rng = rand1.nextInt()
+
+  }
+}
