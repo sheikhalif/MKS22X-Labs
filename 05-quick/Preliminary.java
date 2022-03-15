56933c5d7281086519c89b4cec03d3c0dce6ac8c
Sheikh Alif <salif30@stuy.edu>
2022-03-15 10:01:11 -0400

    added what should happen if all values are equal

--- 05-quick/Preliminary.java
+++ 05-quick/Preliminary.java
@@ -12,9 +12,25 @@ public class Preliminary{
     int startCounter = 0;
     int endCounter = end-start;
     int buffer = 1;
+    boolean coinFlip = true;
     for (int i = 0; i < end-start+1; i++){
       if (buffer == 1 && data[i+start] == data[rng]){
         buffer--;
+      }
+      else if (i > 0 && data[i+start] == data[i+start-1]){
+        if (coinFlip){
+          dataCopy[endCounter] = data[i+start];
+          endCounter--;
+          dataCopy[pivot] = data[rng];
+          coinFlip = false;
+        }
+        else{
+          dataCopy[startCounter] = data[i+start];
+          startCounter++;
+          dataCopy[pivot+1] = data[rng];
+          pivot++;
+          coinFlip = true;
+        }
       }
       else if (data[i+start] >= data[rng]){
         dataCopy[endCounter] = data[i+start];
@@ -36,8 +52,26 @@ public class Preliminary{
     return pivot+start;
   }
   public static void main(String args[]){
+    /**
     int[] example1 = new int[]{0, 0, 0, 32, 51, 57, 64, 12, 12, 0, 0};
     System.out.println(partition(example1, 3, 8));
     System.out.println(Arrays.toString(example1));
+    int [] data = new int[] {4,3,2,1,0};
+    System.out.println("Original: "+Arrays.toString(data));
+    int pivot = partition( data, 0, 4);
+    System.out.println("Pivot value: "+data[pivot]+ ", Pivot index: "+pivot);
+    System.out.println("Modified: "+Arrays.toString(data));
+    System.out.println();
+    **/
+
+    int[] example2 = new int[]{0, 0, 0, 0, 0, 0};
+    System.out.println(partition(example2, 0, 4));
+    System.out.println(Arrays.toString(example2));
+
+    int[] example3 = new int[]{3, 11, 17, 24, 31, 43};
+    System.out.println(partition(example3, 0, 5));
+    System.out.println(Arrays.toString(example3));
+
+
   }
 }
