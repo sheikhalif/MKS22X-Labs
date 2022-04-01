21592d2d6342fba44fc7de6d888c5251db2e2789
Sheikh Alif <salif30@stuy.edu>
2022-03-31 22:36:09 -0400

    finished writing the function, will now test it

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -1,6 +1,59 @@
+import java.util.*;
+
 public class Calculator{
-  public static double eval (String s){
-    Deque<Double> queue = new Deque<Double>();
+  public static double eval (String s) throws IllegalArgumentException{
+    ArrayDeque<Double> deque = new ArrayDeque<Double>();
     String[] inputs = s.split(" ");
+    for (int i = 0; i < inputs.length; i++){
+      if (inputs[i] == "+"){
+        if (deque.size() < 2){
+          throw new IllegalArgumentException("Too many operations");
+        }
+        double second = deque.pollLast();
+        double first = deque.pollLast();
+        deque.add(first+second);
+      }
+      else if(inputs[i] == "-"){
+        if (deque.size() < 2){
+          throw new IllegalArgumentException("Too many operations");
+        }
+        double second = deque.pollLast();
+        double first = deque.pollLast();
+        deque.add(first-second);
+      }
+      else if(inputs[i] == "*"){
+        if (deque.size() < 2){
+          throw new IllegalArgumentException("Too many operations");
+        }
+        double second = deque.pollLast();
+        double first = deque.pollLast();
+        deque.add(first*second);
+      }
+      else if(inputs[i] == "/"){
+        if (deque.size() < 2){
+          throw new IllegalArgumentException("Too many operations");
+        }
+        double second = deque.pollLast();
+        double first = deque.pollLast();
+        deque.add(first/second);
+      }
+      else if (inputs[i] == "%"){
+        if (deque.size() < 2){
+          throw new IllegalArgumentException("Too many operations");
+        }
+        double second = deque.pollLast();
+        double first = deque.pollLast();
+        if (second < 0)second*=-1;
+        if (first < 0)first*=-1;
+        deque.add(first % second);
+      }
+      else{
+        queue.add((double)inputs[i]);
+      }
+    }
+    if (deque.size() > 1){
+      throw new IllegalArgumentException("Not enough operations");
+    }
+    return deque.getFirst();
   }
 }
